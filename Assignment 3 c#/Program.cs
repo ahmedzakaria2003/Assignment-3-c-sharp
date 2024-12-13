a9ce1c51cdb32395003c7893aca46ba5d374358a
Zeko <you@example.com>
2024-12-13 22:05:53 +0200

    Add project files.

---
+++ Assignment 3 c#/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Assignment_3_c_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
